9ca9acb5f4afd9da2b7af16150c73a589fd30f4b
anna <anna@pixnet.tw>
2017-06-12 14:27:34 +0800

    deploy assets

--- src/router/router.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Router, Route } from 'react-router';
-import App from '../App.js';
-
-export default function({history}) {
-    return (<Router history={history}>
-        <Route path="/" component={App}></Route>
-    </Router>);
-}
--- src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
-
-import rootReducer from '../reducers/index.js';
-
-export default function(initialState) {
-    return createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(thunk)));
-}
